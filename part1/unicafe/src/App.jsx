e6f2869022e66bff14f672684f95a0b1f802779d
margaritaintercap <margarita.dzerina@intercap.com>
2025-01-10 18:21:08 +0200

    task 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,10 +8,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    const updatedAll = updatedGood + neutral + bad
+    setAll(updatedAll)
+    const updatedAverage = (updatedGood - bad) / updatedAll
+    setAverage(updatedAverage)
+    const updatedPositive = (updatedGood/updatedAll) * 100
+    setPositive(updatedPositive)
+  }
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    const updatedAll = good + updatedNeutral + bad
+    setAll(updatedAll)
+    const updatedAverage = (good - bad) / updatedAll
+    setAverage(updatedAverage)
+    const updatedPositive = (good/updatedAll) * 100
+    setPositive(updatedPositive)
+  }
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    const updatedAll = good + neutral + updatedBad
+    setAll(updatedAll)
+    const updatedAverage = (good - updatedBad) / updatedAll
+    setAverage(updatedAverage)
+    const updatedPositive = (good/updatedAll) * 100
+    setPositive(updatedPositive)
+  }
 
   return (
     <div>
@@ -22,9 +52,12 @@ const App = () => {
       <Title title={title2} />
 
       <p>
-      {"good"} {good} <br /> 
-      {"neutral"} {neutral} <br /> 
-      {"bad"} {bad}
+      {"good"} {good} <br/> 
+      {"neutral"} {neutral} <br/> 
+      {"bad"} {bad} <br/> 
+      {"all"} {all} <br/> 
+      {"average"} {average} <br/> 
+      {"positive"} {positive} %
       </p>
 
     </div>
