cb4a4bd1b0cc56777b13103386170d03c6cecad7
margaritaintercap <margarita.dzerina@intercap.com>
2025-01-27 16:35:05 +0200

    task 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,49 @@
+const Header = (props) => <h1>{props.course.name}</h1>
+const Part = (props) => <p> {props.part.name} {props.part.exercises}</p>
+const Content = (props) => {
+  const partsArray = props.course.parts
+  return (
+    partsArray.map(part =>
+      <div key={part.id}>
+        <Part part={part}/>
+      </div>
+    )
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course} />
+      <Content course={props.course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
