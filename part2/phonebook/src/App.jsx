5dcc232c418b835683091d47ae020e2a5c9f4150
margaritaintercap <margarita.dzerina@gmail.com>
2025-02-14 15:30:08 +0200

    task 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import personService from "./services/persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newFilter, setNewFilter] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
+    });
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -22,15 +24,11 @@ const App = () => {
       id: persons.length + 1,
     };
 
-    if (newName && newNumber) {
-      persons.some((person) => person.name === personObject.name)
-        ? alert(`${personObject.name} name is already added to phonebook`)
-        : setPersons(persons.concat(personObject));
+    personService.create(personObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
       setNewName("");
       setNewNumber("");
-    } else {
-      alert("Enter both name and number");
-    }
+    });
   };
 
   const contactsToShow = newFilter
