26fd1b2a91a65c44e80f81e2216b6503c309c116
Margarita121 <margarita.dzerina@gmail.com>
2025-03-09 13:46:27 +0200

    fixed port number

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -73,7 +73,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = process.env.PORT || 3001b
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
