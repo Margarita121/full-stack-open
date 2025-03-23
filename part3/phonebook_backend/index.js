809ca1ca9cad3381e162aefcef28b75df7b2f15f
Margarita121 <margarita.dzerina@gmail.com>
2025-03-23 18:46:11 +0200

    task 3.14

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,100 +1,125 @@
+require("dotenv").config();
 const express = require("express");
-var morgan = require("morgan");
+// var morgan = require("morgan");
 const app = express();
+const Person = require("./models/person");
 
 app.use(express.json());
 app.use(express.static('dist'))
 
-morgan.token("data", function (req, res) {
-  return JSON.stringify(req.body);
-});
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :data")
-);
+// morgan.token("data", function (req, res) {
+//   return JSON.stringify(req.body);
+// });
+// app.use(
+//   morgan(":method :url :status :res[content-length] - :response-time ms :data")
+// );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: "1",
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: "2",
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: "3",
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: "4",
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
-app.get("/info", (request, response) => {
-  response.send(
-    `<div>Phonebook has info for ${
-      persons.length
-    } people</div><div>${Date()}</div>`
-  );
-});
+// app.get("/info", (request, response) => {
+//   response.send(
+//     `<div>Phonebook has info for ${
+//       persons.length
+//     } people</div><div>${Date()}</div>`
+//   );
+// });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  // response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
+  
+  // const id = request.params.id;
+  // const person = persons.find((person) => person.id === id);
+  // if (person) {
+  //   response.json(person);
+  // } else {
+  //   response.status(404).end();
+  // }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = request.params.id;
+//   persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
-});
+//   response.status(204).end();
+// });
 
-const generateId = () => {
-  return String(Math.floor(Math.random() * 999));
-};
+// const generateId = () => {
+//   return String(Math.floor(Math.random() * 999));
+// };
+
+// app.post("/api/persons", (request, response) => {
+//   const body = request.body;
+
+//   const person = {
+//     id: generateId(),
+//     name: body.name,
+//     number: body.number,
+//   };
+
+//   const existingPerson = persons.find((p) => p.name === person.name);
+//   if (!body.name || !body.number) {
+//     return response.status(400).json({
+//       error: "Missing contact fields",
+//     });
+//   }
+//   if (existingPerson) {
+//     return response.status(400).json({
+//       error: "Name must be unique",
+//     });
+//   }
+
+//   persons = persons.concat(person);
+//   response.json(person);
+// });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  const person = {
-    id: generateId(),
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: "Missing contact fields" });
+  }
+
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  const existingPerson = persons.find((p) => p.name === person.name);
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "Missing contact fields",
-    });
-  }
-  if (existingPerson) {
-    return response.status(400).json({
-      error: "Name must be unique",
-    });
-  }
-
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ part3/phonebook_backend/mango.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+const inputName = process.argv[3];
+const inputNumber = process.argv[4];
+
+const url = `mongodb+srv://margaritad1212:${password}@cluster0.z49ui.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: inputName,
+  number: inputNumber,
+});
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+} else {
+  person.save().then((result) => {
+    console.log(`added ${inputName} number ${inputNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+}
