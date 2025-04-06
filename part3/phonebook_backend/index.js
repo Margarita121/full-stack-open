2924e9f3f678605eff4de96018c57c84f4da2fd9
Margarita121 <margarita.dzerina@gmail.com>
2025-04-06 11:22:59 +0300

    uibuild

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -44,25 +44,25 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.put('/api/persons/:id', (request, response, next) => {
-  const { number } = request.body
+app.put("/api/persons/:id", (request, response, next) => {
+  const { number } = request.body;
 
   Person.findById(request.params.id)
-    .then(person => {
+    .then((person) => {
       if (!person) {
-        return response.status(404).end()
+        return response.status(404).end();
       }
 
-      person.number = number
+      person.number = number;
 
       return person.save().then((updatedPerson) => {
-        response.json(updatedPerson)
-      })
+        response.json(updatedPerson);
+      });
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -74,12 +74,14 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-//error handling works with backend requests but not frontend
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
@@ -91,6 +93,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
