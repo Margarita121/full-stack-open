15dda7524e132b3fcff8ea095f49bd20360f6292
Margarita121 <margarita.dzerina@gmail.com>
2025-04-10 16:57:24 +0300

    uibuild

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -55,10 +55,12 @@ app.put("/api/persons/:id", (request, response, next) => {
 
       person.number = number;
 
-      return person.save().then((updatedPerson) => {
-        response.json(updatedPerson);
-      })
-      .catch((error) => next(error))
+      return person
+        .save()
+        .then((updatedPerson) => {
+          response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
     })
     .catch((error) => next(error));
 });
