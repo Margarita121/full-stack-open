a694916125db2934196b9141ca2d5f3218ade921
Margarita121 <margarita.dzerina@gmail.com>
2025-06-16 16:00:18 +0300

    task 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -20,6 +20,15 @@ const App = () => {
     }
   }, [user])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
       event.preventDefault()
       
@@ -28,7 +37,7 @@ const App = () => {
           username, password,
         })
         window.localStorage.setItem(
-          'loggedNoteappUser', JSON.stringify(user)
+          'loggedBlogappUser', JSON.stringify(user)
         ) 
         blogService.setToken(user.token)
         setUser(user)
@@ -43,6 +52,11 @@ const App = () => {
       }
     }
 
+    const handleLogout = async () => {
+      window.localStorage.removeItem('loggedBlogappUser')
+      window.location.reload()
+    }
+
   return (
     <div>
       <Notification message={errorMessage} />
@@ -50,7 +64,9 @@ const App = () => {
       <LoginForm handleLogin={handleLogin} username={username} setUsername={setUsername} password={password} setPassword={setPassword} /> :
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged-in</p>
+        <p>
+          {user.name} logged-in <button onClick={handleLogout}>logout</button>
+        </p> 
         {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
         )}
