c65001b5de3f0464b151a267d23fca0e1948b34d
Margarita121 <margarita.dzerina@gmail.com>
2025-07-29 11:07:00 +0300

    added prettier

--- part7/anecdotes-class/src/App.jsx
+++ part7/anecdotes-class/src/App.jsx
@@ -7,20 +7,18 @@ class App extends React.Component {
 
     this.state = {
       anecdotes: [],
-      current: 0
+      current: 0,
     }
   }
 
   componentDidMount = () => {
-    axios.get('http://localhost:3001/anecdotes').then(response => {
+    axios.get('http://localhost:3001/anecdotes').then((response) => {
       this.setState({ anecdotes: response.data })
     })
   }
 
   handleClick = () => {
-    const current = Math.floor(
-      Math.random() * this.state.anecdotes.length
-    )
+    const current = Math.floor(Math.random() * this.state.anecdotes.length)
     this.setState({ current })
   }
 
