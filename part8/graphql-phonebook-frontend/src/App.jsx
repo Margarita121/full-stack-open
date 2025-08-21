6a2c975af98d8b02360440f18f5e3f003e434c59
Margarita121 <margarita.dzerina@gmail.com>
2025-08-21 11:57:37 +0300

    part8e

--- part8/graphql-phonebook-frontend/src/App.jsx
+++ part8/graphql-phonebook-frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { useQuery, useApolloClient } from '@apollo/client'
+import { useQuery, useApolloClient, useSubscription } from '@apollo/client'
 import { useState } from "react";
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm';
-import { ALL_PERSONS } from './queries'
+import { ALL_PERSONS, PERSON_ADDED } from './queries'
 import LoginForm from './components/LoginForm';
 
 const Notify = ({errorMessage}) => {
@@ -18,11 +18,37 @@ const Notify = ({errorMessage}) => {
   )
 }
 
+// function that takes care of manipulating cache
+export const updateCache = (cache, query, addedPerson) => {
+  // helper that is used to eliminate saving same person twice
+  const uniqByName = (a) => {
+    let seen = new Set()
+    return a.filter((item) => {
+      let k = item.name
+      return seen.has(k) ? false : seen.add(k)
+    })
+  }
+
+  cache.updateQuery(query, ({ allPersons }) => {
+    return {
+      allPersons: uniqByName(allPersons.concat(addedPerson)),
+    }
+  })
+}
+
 const App = () => {
   const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS, {})
   const client = useApolloClient()
+
+  useSubscription(PERSON_ADDED, {
+    onData: ({ data, client }) => {
+      const addedPerson = data.data.personAdded
+      notify(`${addedPerson.name} added`)
+      updateCache(client.cache, { query: ALL_PERSONS }, addedPerson)
+    }
+  })
 
   if (result.loading)  {
     return <div>loading...</div>
