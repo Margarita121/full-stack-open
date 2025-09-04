c97765fc07380e65d2c588bc23037cece8560113
Margarita121 <margarita.dzerina@gmail.com>
2025-09-04 16:12:16 +0300

    tasks 9.4-9.5

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -17,28 +17,39 @@ const parseArguments = (args: string[]): CalculateBmi => {
   }
 }
 
-const bmiResult = (heightCm: number, massKg: number) => {
+const bmiResult = (heightCm: number, massKg: number): string => {
   const bmi = massKg / ((heightCm / 100) ** 2)
   switch (true) {
-    case (bmi < 18.5): 
-      console.log(`underweight, bmi value: ${bmi}`)
-      break
+    case (bmi > 0 && bmi < 18.5): 
+      return `underweight, bmi value: ${bmi}`
     case (bmi >= 18.5 && bmi < 25): 
-      console.log(`normal range, bmi value: ${bmi}`)
-      break
-    case (bmi >= 25): 
-      console.log(`overweight, bmi value: ${bmi}`)
-      break
+      return `normal range, bmi value: ${bmi}`
+    case (bmi >= 25 && bmi < 100): 
+      return `overweight, bmi value: ${bmi}`
+    default:
+      return 'malformatted parameters'
   }
 }
 
-try {
-  const { value1, value2 } = parseArguments(process.argv);
-  bmiResult(value1, value2);
+if (require.main === module) {
+    const { value1, value2 } = parseArguments(process.argv);
+    console.log(bmiResult(value1, value2))
+  }
+
+export const bmi = (height:number, weight:number) => {
+  try {
+  const result = {
+    weight: weight,
+    height: height,
+    bmi: bmiResult(height, weight)
+  }
+  return result;
 } catch (error: unknown) {
   let errorMessage = 'Something bad happened.'
   if (error instanceof Error) {
     errorMessage += ' Error: ' + error.message;
   }
   console.log(errorMessage);
+  return { error: errorMessage}
+}
 }
---
+++ part9/bmi/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { bmi } from './bmiCalculator'
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = req.query.height
+  const weight = req.query.weight
+  res.send(bmi(Number(height), Number(weight)));
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
