ce543e00c50d309c1e44c08f10d7bc1bcdfa29ca
Margarita121 <margarita.dzerina@gmail.com>
2025-09-19 16:49:16 +0300

    task 9.15

---
+++ part9/vite-with-typescript/src/components/Content.tsx
@@ -0,0 +1,17 @@
+interface CoursePart {
+  name: string,
+  exerciseCount: number
+}
+interface ContentProps {
+  courseParts: CoursePart[]
+}
+const Content = (props: ContentProps) => (
+  <div>
+    {props.courseParts.map((part) => (
+      <p key={part.name}>
+        {part.name} {part.exerciseCount}
+      </p>
+    ))}
+  </div>
+)
+export default Content;
